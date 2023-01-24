14775a4ba146d5b575a185ee1d070c53eb48b997
Ebner Silva <ebner.suporte@hotmail.com>
2023-01-23 23:48:22 -0300

    feat: first commit

---
+++ Netflix Clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadsViewController.swift
+//  Netflix Clone
+//
+//  Created by Ebner Silva on 23/01/23.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Ebner Silva on 23/01/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
